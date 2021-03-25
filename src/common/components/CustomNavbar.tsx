0d2dda268328fce01a8672910b747e89062f7804
Aaron Pradhan <12a.pradhan@gmail.com>
2021-03-25 12:43:59 -0400

    Fix href and remove fixed attr from Navbar

--- src/common/components/CustomNavbar.tsx
+++ src/common/components/CustomNavbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Form, FormControl, Button, Nav } from 'react-bootstrap'
+import {Navbar, Form, FormControl, Button, Nav} from 'react-bootstrap'
 
 interface CustomNavbarProps {
     loggedIn: boolean;
@@ -7,28 +7,28 @@ interface CustomNavbarProps {
 }
 
 export default class CustomNavbar extends React.Component<CustomNavbarProps> {
-  render() {
-    return (
-      <Navbar fixed="top" style={{backgroundColor: "#6eaedd"}} variant="dark" className="museo-300">
-        <Navbar.Brand href="#home">
-          <p className="museo-display-black m-0">Spotify Collaborative Playlists</p>
-          </Navbar.Brand>
-        <Nav className="mr-auto">
-          <Form inline >
-            <FormControl type="text" placeholder="Type here..." className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </Form>
-        </Nav>
-          {
-              this.props.loggedIn ?
-                  <Navbar.Text>
-                      Logged in as {this.props.displayName}
-                  </Navbar.Text> :
-                  <Button variant="outline-light">
-                      Log in with Spotify
-                  </Button>
-          }
-      </Navbar>
-    )
-  }
+    render() {
+        return (
+            <Navbar style={{backgroundColor: "#6eaedd"}} variant="dark" className="museo-300">
+                <Navbar.Brand href="#">
+                    <p className="museo-display-black m-0">Spotify Collaborative Playlists</p>
+                </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Form inline>
+                        <FormControl type="text" placeholder="Type here..." className="mr-sm-2"/>
+                        <Button variant="outline-light">Search</Button>
+                    </Form>
+                </Nav>
+                {
+                    this.props.loggedIn ?
+                        <Navbar.Text>
+                            Logged in as {this.props.displayName}
+                        </Navbar.Text> :
+                        <Button variant="outline-light">
+                            Log in with Spotify
+                        </Button>
+                }
+            </Navbar>
+        )
+    }
 }
