96fa073a6c093f8c433b328a419f0ca70d7beaaf
Aaron Pradhan <12a.pradhan@gmail.com>
2021-03-25 13:53:22 -0400

    Fix code style

--- src/common/components/CustomNavbar.tsx
+++ src/common/components/CustomNavbar.tsx
@@ -14,6 +14,9 @@ export default class CustomNavbar extends React.Component<CustomNavbarProps> {
                     <p className="museo-display-black m-0">Spotify Collaborative Playlists</p>
                 </Navbar.Brand>
                 <Nav className="mr-auto">
+                    {/*<Nav.Link href="#playlist/abc/requests">*/}
+                    {/*    Requests*/}
+                    {/*</Nav.Link>*/}
                     <Form inline>
                         <FormControl type="text" placeholder="Type here..." className="mr-sm-2"/>
                         <Button variant="outline-light">Search</Button>
