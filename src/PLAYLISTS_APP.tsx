c9d542b9b666dda7cc66bbfb703a7f3fe3e9b9fa
Sid-Singh7 <siddharthsingh539@gmail.com>
2021-03-30 12:16:19 -0400

    Manage admins page and user profile page

--- src/PLAYLISTS_APP.tsx
+++ src/PLAYLISTS_APP.tsx
@@ -7,6 +7,8 @@ import Homepage from './pages/homepage/Homepage';
 import RequestsPage from './pages/requests/RequestsPage';
 
 import './common/css/typography.css'
+import UserProfile from './pages/userprofilepage/UserProfilePage';
+import ManageAdmin from './pages/manageadministrators/ManageAdminPage';
 
 class PLAYLISTS_APP extends React.Component {
     render() {
@@ -18,6 +20,12 @@ class PLAYLISTS_APP extends React.Component {
 
                     {/* In order to access this playlist ID from the pages that need it, you need to use props.match.params.<VARIABLE_NAME> in that component */}
                     <Route path="/playlist/:playlistId/requests" component={RequestsPage} exact/>
+
+                    {/* Route to User profile page */}
+                    <Route path="/user/:username" component={UserProfile} exact/>
+
+                    {/* Route to User profile page */}
+                    <Route path="/playlist/:playlistid/admins" component={ManageAdmin} exact/>
                 </Router>
             </Container>
         );
