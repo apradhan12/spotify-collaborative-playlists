875a81fe06240d90dc6c346b9497296676609333
Sid-Singh7 <siddharthsingh539@gmail.com>
2021-03-30 12:50:57 -0400

    Fix modal

--- src/pages/manageadministrators/index.tsx
+++ src/pages/manageadministrators/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container, Row } from "react-bootstrap";
+import { Modal, Button, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import {playlistMap} from "../../data";
 
