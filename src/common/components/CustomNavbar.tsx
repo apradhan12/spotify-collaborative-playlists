55417d12d77ebc786bb12197b3c7a3f9d228b602
Aaron Pradhan <12a.pradhan@gmail.com>
2021-03-31 18:33:16 -0400

    add logout button

--- src/common/components/CustomNavbar.tsx
+++ src/common/components/CustomNavbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Navbar, Button, Nav, Image} from 'react-bootstrap'
-import { User } from '../types'
+import {User} from '../types'
 import PlaylistSearchBar from './PlaylistSearchBar'
 
 interface CustomNavbarProps {
@@ -29,18 +29,22 @@ export default class CustomNavbar extends React.Component<CustomNavbarProps> {
                         Admin
                     </Nav.Link>
                 </Nav>
-                <PlaylistSearchBar placeholder="Search for playlists..." />
+                <PlaylistSearchBar placeholder="Search for playlists..."/>
                 {
                     this.props.user ?
-                        <Navbar.Text>
-                            Logged in as 
-                            <Nav.Link className="d-inline p-0 ml-2" href={`#/user/${this.props.user.username}`}>
+                        <>
+                            <Navbar.Text>
+                                Logged in as
+                                <Nav.Link className="d-inline p-0 ml-2" href={`#/user/${this.props.user.username}`}>
                                 <span>
                                     {this.props.user.displayName}
-                                    <Image fluid src={process.env.PUBLIC_URL + this.props.user.profilePictureURL} style={{maxWidth: "30px", marginLeft: "5px"}} />
+                                    <Image fluid src={process.env.PUBLIC_URL + this.props.user.profilePictureURL}
+                                           style={{maxWidth: "30px", marginLeft: "5px"}}/>
                                 </span>
-                            </Nav.Link>
-                        </Navbar.Text> :
+                                </Nav.Link>
+                            </Navbar.Text>
+                            <Button variant="light" className="ml-5" onClick={() => window.location.reload()}>Log out</Button>
+                        </> :
                         <Button variant="light" onClick={this.props.toggleLoginModal()}>
                             Log in
                         </Button>
